dc50cea416e1c4375f191e607d81ec572c809eac
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2020-07-19 19:44:04 -0700

    proper perspective sorta

--- colosseum/web/index.js
+++ colosseum/web/index.js
@@ -16,18 +16,74 @@ let engine = new Engine();
 let ele = document.createElement("div");
 ele.classList.add("test");
 document.body.append(ele);
-let perspective = 100;
-let angle = 0;
+let model = glMatrix.mat4.fromTranslation(glMatrix.mat4.create(), [0, 0, 0]);
+let playerPos = [0, 0, 300];
+let cameraFront = [0, 0, -1];
+let cameraUp = [0, 1, 0];
+let yaw = -Math.PI/2;
+let pitch = 0;
+let walkSpeed = 10;
+let mouseSensitivity = 0.01;
+let keysPressed = {};
 
 //glMatrix.mat4.perspective(glMatrix.mat4.create(), Math.PI / 8, 1, 0.1, 10)
 function animate() {
     requestAnimationFrame(animate);
-    angle += 0.01;
     
-    ele.style.transform = buildCssTransform(10000, [window.innerWidth / 2, window.innerHeight / 2, 0], [angle, angle, angle]);
+    cameraFront[0] = Math.cos(yaw) * Math.cos(pitch);
+    cameraFront[1] = Math.sin(pitch);
+    cameraFront[2] = Math.sin(yaw) * Math.cos(pitch);
+    cameraFront = glMatrix.vec3.normalize(glMatrix.vec3.create(), cameraFront);
+    let cameraRight = glMatrix.vec3.normalize(glMatrix.vec3.create(), glMatrix.vec3.cross(glMatrix.vec3.create(), cameraFront, [0, 1, 0]));
+    cameraUp = glMatrix.vec3.normalize(glMatrix.vec3.create(), glMatrix.vec3.cross(glMatrix.vec3.create(), cameraRight, cameraFront));
+    let previousY = playerPos[1];
+    if(keysPressed["w"] == true) {
+        playerPos = glMatrix.vec3.add(glMatrix.vec3.create(), playerPos, glMatrix.vec3.scale(glMatrix.vec3.create(), cameraFront, walkSpeed));
+    }
+    if(keysPressed["s"] == true) {
+        playerPos = glMatrix.vec3.subtract(glMatrix.vec3.create(), playerPos, glMatrix.vec3.scale(glMatrix.vec3.create(), cameraFront, walkSpeed));
+    }
+    if(keysPressed["a"] == true) {
+        playerPos = glMatrix.vec3.subtract(glMatrix.vec3.create(), playerPos, glMatrix.vec3.scale(glMatrix.vec3.create(), cameraRight, walkSpeed));
+    }
+    if(keysPressed["d"] == true) {
+        playerPos = glMatrix.vec3.add(glMatrix.vec3.create(), playerPos, glMatrix.vec3.scale(glMatrix.vec3.create(), cameraRight, walkSpeed));
+    }
+    playerPos[1] = previousY;
+    let view = glMatrix.mat4.lookAt(glMatrix.mat4.create(), playerPos, glMatrix.vec3.add(glMatrix.vec3.create(), playerPos, cameraFront), cameraUp);
+    let mvp = glMatrix.mat4.multiply(glMatrix.mat4.create(), view, model);
+    ele.style.transform = mat4ToCss(window.innerWidth / 2, mvp);
 }
+
 requestAnimationFrame(animate);
 
-function buildCssTransform(perspective, translation, rotation) {
-    return `perspective(${perspective}px) translate(-50%, -50%) translate3d(${translation[0]}px,${translation[1]}px,${translation[2]}px) rotateX(${rotation[0]}rad) rotateY(${rotation[1]}rad) rotateZ(${rotation[2]}rad)`;
+function mat4ToCss(perspective, matrix) {
+    let out = `perspective(${perspective}px) matrix3d(`;
+    for(let i = 0; i < 15; i++) {
+        out += matrix[i] + ",";
+    }
+    out += matrix[15] + ")";
+    return out;
+}
+
+document.onkeydown = (e) => {
+    keysPressed[e.key] = true;
+};
+
+document.onkeyup = (e) => {
+    keysPressed[e.key] = false;
+};
+
+document.onclick = () => {
+    document.body.requestPointerLock = document.body.requestPointerLock || document.body.mozRequestPointerLock;
+    document.body.requestPointerLock();
+}
+
+document.onmousemove = (e) => {
+    if(document.pointerLockElement === document.body || document.mozPointerLockElement === document.body) {
+        yaw += mouseSensitivity * e.movementX;
+        pitch += mouseSensitivity * e.movementY;
+        if(pitch >  Math.PI / 2 - 0.01) pitch =  Math.PI / 2 - 0.01;
+        if(pitch < -Math.PI / 2 + 0.01) pitch = -Math.PI / 2 + 0.01;
+    }
 }
